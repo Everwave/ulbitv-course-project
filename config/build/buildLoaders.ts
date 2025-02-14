ccf8ee2c8c8edf727ac286ee5ca6f1e83a74462f
everwave <describeeternity@gmail.com>
2025-02-15 02:58:45 +0300

    init

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,29 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { RuleSetRule }  from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: true
+        }
+      },
+      "sass-loader",
+    ]
+  }
+
+  const typescriptLoader =  {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  }
+ 
+  return  [
+    typescriptLoader, cssLoader
+  ]
+}
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFn(a: number): string {
+  console.log('heheheh')
+  
+  return ''
+}
